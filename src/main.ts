aa538372955a001378bce861858e0615437700a9
RocioAV <rocio.velazquez.0212@gmail.com>
2026-06-16 20:16:06 -0300

    Se implemento CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
+
+  // CORS
+  app.enableCors({
+    // origin: Define quién tiene permiso de entrar. Solo la URL exacta que pusimos pasará el filtro.
+    origin: frontendUrl, 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
