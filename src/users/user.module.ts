d716574d47635ed2c6dc4941f5ab41b4d5b1a97a
estebankaraputny <estebankaraputny18@gmail.com>
2026-07-01 12:20:14 -0300

    feat: implement JWT authentication strategy and configure UserModule with dependencies

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -2,7 +2,6 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserService } from './user.service'; 
 import { UserController } from './user.controller';
 import { RequestIdMiddleware } from 'src/common/middleware/request-id.middleware';
-import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 import { ConfigModule } from '@nestjs/config'
 
 @Module({
